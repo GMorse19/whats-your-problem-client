1074e11cba562cacddc51e584b1059d136a63196
GMorse19 <malorse@comcast.net>
2020-02-27 21:56:51 -0500

    Added guess feature

--- src/components/Problem/ProblemShow.js
+++ src/components/Problem/ProblemShow.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 
@@ -7,6 +8,7 @@ import apiUrl from '../../apiConfig'
 
 const Problem = props => {
   const [problem, setProblem] = useState(null)
+  const [guess, setGuess] = useState({ answer: '' })
   const userId = props.user ? props.user_id : null
   useEffect(() => {
     axios({
@@ -34,6 +36,21 @@ const Problem = props => {
       })
   }
 
+  const handleSubmit = event => {
+    console.log('loser')
+
+    event.preventDefault()
+
+    if (guess.answer === problem.answer) {
+      console.log('winner winner')
+    }
+  }
+
+  const handleChange = event => {
+    event.persist()
+    setGuess({ ...guess, [event.target.name]: event.target.value })
+  }
+
   if (!problem) {
     return <p>Loading...</p>
   }
@@ -45,6 +62,7 @@ const Problem = props => {
       <h6>Category: {problem.category}</h6>
       <h6>ID: {problem.id}</h6>
       <h6>User: {problem.user.email}</h6>
+      <h6>Answer: {problem.answer}</h6>
       <div>
         {userId === problem.user_id && <Button
           href={`#problems/${props.match.params.id}/update`}
@@ -59,6 +77,19 @@ const Problem = props => {
           className="btn btn-danger">
           delete
         </Button>}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label htmlFor="name">Place an answer below</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Answer Here..."
+              value={guess.answer}
+              name="answer"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
       </div>
     </div>
   )
