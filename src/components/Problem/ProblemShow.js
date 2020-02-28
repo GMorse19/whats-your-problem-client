32a78ac171ce24dccacc55f20ebf58686bea714c
GMorse19 <malorse@comcast.net>
2020-02-27 23:21:24 -0500

    Added modals for wins and losses

--- src/components/Problem/ProblemShow.js
+++ src/components/Problem/ProblemShow.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
 import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 
@@ -9,6 +10,9 @@ import apiUrl from '../../apiConfig'
 const Problem = props => {
   const [problem, setProblem] = useState(null)
   const [guess, setGuess] = useState({ answer: '' })
+  const [showWin, setShowWin] = useState(false)
+  const [showLoss, setShowLoss] = useState(false)
+
   const userId = props.user ? props.user_id : null
   useEffect(() => {
     axios({
@@ -43,6 +47,9 @@ const Problem = props => {
 
     if (guess.answer === problem.answer) {
       console.log('winner winner')
+      handleShowWin()
+    } else {
+      handleShowLoss()
     }
   }
 
@@ -51,12 +58,51 @@ const Problem = props => {
     setGuess({ ...guess, [event.target.name]: event.target.value })
   }
 
+  const handleShowWin = () => setShowWin(true)
+  const handleCloseWin = () => {
+    setShowWin(false)
+    setGuess({ answer: '' })
+  }
+  const handleShowLoss = () => setShowLoss(true)
+  const handleCloseLoss = () => setShowLoss(false)
+
   if (!problem) {
     return <p>Loading...</p>
   }
+
   console.log(problem)
   return (
     <div className="problem-board">
+      <Modal className="modal-dialog" show={showWin} onHide={handleCloseWin}>
+        <Modal.Header closeButton>
+          <Modal.Title>You Won! The answer is {problem.answer}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Blah Blah</Modal.Body>
+        <Modal.Footer>
+          <Modal.Title>Good Job!</Modal.Title>
+          <Button variant="secondary" onClick={handleCloseWin}>
+            Close
+          </Button>
+          <Button variant="primary" as={'a'} href={'#/problems'}>
+            Try Another
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal className="modal-dialog" show={showLoss} onHide={handleCloseLoss}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sorry, You lost. Please try again.</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Blah Blah</Modal.Body>
+        <Modal.Footer>
+          <Modal.Title>Sorry.</Modal.Title>
+          <Button variant="secondary" onClick={handleCloseLoss}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseLoss}>
+            Try Again
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <h1>{problem.name}</h1>
       <h4>Content: {problem.content}</h4>
       <h6>Category: {problem.category}</h6>
