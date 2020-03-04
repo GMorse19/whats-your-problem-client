e3a7c14e678fa1ca2bb9e1be8c372dc5305e7b4d
GMorse19 <malorse@comcast.net>
2020-03-03 20:11:03 -0500

    Added a temp home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import './Home.scss'
+
 const Home = () => {
   return (
-    <div style={{ textAlign: 'center' }}>HOME</div>
+    <div>
+      <Container className="home-container">
+        <Row className="box">
+          <Col className="box">
+            <img
+              style={{ justifyContent: 'center' }}
+              className="box"
+              src="WYP.png"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 export default Home
