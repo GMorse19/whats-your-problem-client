636d159a7b046ad935bd15d536ec14b57e81b09e
GMorse19 <malorse@comcast.net>
2020-06-24 23:20:20 -0400

    Basic form styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
+import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+
+import './Search.scss'
+
 const Search = ({ find, setFilter }) => {
   const handleChange = event => {
     event.persist()
@@ -12,8 +17,14 @@ const Search = ({ find, setFilter }) => {
   const fil = []
 
   return (
-    <div>
-      <input onChange={handleChange} />
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Form.Group style={{ width: '50vw' }}>
+        <Form.Control
+          className='search-input search-info'
+          onChange={handleChange}
+          placeholder='search'
+        />
+      </Form.Group>
     </div>
   )
 }
