e529ea2e5188b2d7bbf8c6072d2f072a704e2561
GMorse19 <malorse@comcast.net>
2020-08-12 12:30:25 -0400

    Implemented random helper

---
+++ src/components/ModalForm/messages.js
@@ -0,0 +1,15 @@
+const winTitle = 'You Won! The answer is '
+const winBody = ['You are on a roll!', 'Try Another']
+const winFooter = ['Good Job!', 'Great Job!', 'Tremendous!']
+const loseTitle = 'Sorry. Better luck next time!!!'
+const loseBody = 'Sorry, You lost. Please try again.'
+const loseFooter = 'You can do it!'
+
+export default {
+  winTitle: winTitle,
+  winBody: winBody,
+  winFooter: winFooter,
+  loseTitle: loseTitle,
+  loseBody: loseBody,
+  loseFooter: loseFooter
+}
