f9627a1bd090e0152e15a396cbb0a4091f98f056
GMorse19 <malorse@comcast.net>
2020-08-13 21:08:05 -0400

    Added messages and corrected loseBody placement

--- src/components/ModalForm/messages.js
+++ src/components/ModalForm/messages.js
@@ -1,9 +1,9 @@
-const winTitle = ['You Won! The answer is ']
-const winBody = ['You are on a roll!', 'Try Another']
-const winFooter = ['Good Job!', 'Great Job!', 'Tremendous!']
-const loseTitle = ['Sorry. Better luck next time.', 'Not quite right.']
-const loseBody = ['Sorry, You lost.', 'Please try again.']
-const loseFooter = ['You can do it!', 'Keep at it!']
+const winTitle = ['You Won! The answer is ', 'You got it! The answer is ', 'The answer is ']
+const winBody = ['You are on a roll!', 'Try Another One', 'Don\'t stop now! Try another!', 'You\'re a Math wizard!']
+const winFooter = ['Good Job!', 'Great Job!', 'Tremendous!', 'Excellent!']
+const loseTitle = ['Sorry. Better luck next time.', 'Not quite right.', 'Ooops. That\'s not correct.']
+const loseBody = ['There can only be one correct answer.', 'Please try again.', 'Be sure to double check your work.']
+const loseFooter = ['You can do it!', 'Keep at it!', 'Give it another shot.', 'You got this.']
 
 export default {
   winTitle: winTitle,
